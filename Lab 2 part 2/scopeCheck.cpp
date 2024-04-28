68dc7dfb567b2260c5946347a686d56ce060dc48
Parth <parthpatto@gmail.com>
2024-04-28 13:40:12 +0530

    scopeCheck added more swtiches

--- Lab 2 part 2/scopeCheck.cpp
+++ Lab 2 part 2/scopeCheck.cpp
@@ -58,7 +58,97 @@ void SymbolTable::exit_scope() {
 }
 
 bool SymbolTable::check_node(ASTNode* node, bool declared) {
-    
+
+    ASTNode::node_types ntype = node->type;
+    std::cout << "NODE TYPE: " << ntype << std::endl;
+    if (ntype == ASTNode::node_types::TRANSLATION_UNIT){
+        bool b = true;
+        for (auto child : node->children) {
+            b = b && check_node(child, declared);
+        }
+        return b;
+    }
+    else if (ntype == ASTNode::node_types::FUNC_NAME) {
+        bool b = true;
+        for (auto child : node->children) {
+            b = b && check_node(child, declared);
+        }
+        return b;
+    }
+    else if (ntype == ASTNode::node_types::FUNCDEF){
+        std::string fName = node->children[0]->val;
+        // std::cout << "HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEE " << fName << std::endl; 
+        if (scopes[scopes.size() - 1].check_scope(fName, "function")) {
+            std::cout << "Function " << fName << " already declared." << std::endl;
+            return false;
+        }
+        else {
+            scopes[scopes.size() - 1].add_symbol(fName, "function");
+            enter_scope();
+            bool b = true;
+            for (int i = 1; i < node->children.size(); i++) {
+                b = b && check_node(node->children[i], true);
+            }
+            exit_scope();
+            return b;
+        }
+    }
+    else if (ntype == ASTNode::node_types::BLOCK) {
+        enter_scope();
+        bool b = true;
+        for (auto child : node->children) {
+            b = b && check_node(child, declared);
+        }
+        exit_scope();
+        return b;
+    }
+    else if (ntype == ASTNode::node_types::VARDEC) {
+        std::string vName = node->children[0]->val;
+        if (scopes[scopes.size() - 1].check_scope(vName, "variable")) {
+            std::cout << "Variable " << vName << " already declared." << std::endl;
+            return false;
+        }
+        else {
+            scopes[scopes.size() - 1].add_symbol(vName, "variable");
+            return true;
+        }
+    }
+    else if (ntype == ASTNode::node_types::FOR) {
+        bool b = true;
+        for (auto child : node->children) {
+            b = b && check_node(child, declared);
+        }
+        if (b) {
+            enter_scope();
+            b = check_node(node->children[1], declared);
+            exit_scope();
+            return b;
+        }
+        else return false;
+    }
+    else if (ntype == ASTNode::node_types::WHILE) {
+        bool b = true;
+        for (auto child : node->children) {
+            b = b && check_node(child, declared);
+        }
+        if (b) {
+            enter_scope();
+            b = check_node(node->children[1], declared);
+            exit_scope();
+            return b;
+        }
+        else return false;
+    }
+    else {
+        bool b = true;
+        for (auto child : node->children) {
+            b = b && check_node(child, declared);
+        }
+        return b;
+    }
+
+    return true;
+
 }
 
 
